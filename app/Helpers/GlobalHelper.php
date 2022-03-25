5280bb8fca0a8d7384206b43a251b2a3998953cc
daffyyyy <41084667+daffyyyy@users.noreply.github.com>
2022-03-25 21:37:13 +0100

    Update GlobalHelper.php

--- app/Helpers/GlobalHelper.php
+++ app/Helpers/GlobalHelper.php
@@ -1,6 +1,6 @@
 <?php
 
-function linkify(string $text) {
+function linkify(string $text): string {
     return preg_replace('@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#-]*(\?\S+)?[^\.\s])?)?)@', '<a href="$1" target="_blank">$1</a>', $text);
   }
   
